8542062169e2e6515da9c71772d410ecaa9748b7
apa0 <157389416+apa0@users.noreply.github.com>
2024-05-15 12:42:26 -0400

    fixed some scope errors

--- Category.cpp
+++ Category.cpp
@@ -1,9 +1,11 @@
-//Angie Diaz Summer 2024
+//Angie Diaz
+//Summer 2024
+//To-Do List Project: Category.cpp
 
 #include "Category.h"
 
 //method that adds a new Task to goals list, might want to add another parameter to take in a deadline 
-void addTask(std::string t) {
+void Category::addTask(std::string t) {
     //need to instantiate a task object here
     if (!taskExist(t)) {
         Task goal(t);
@@ -15,7 +17,7 @@ void addTask(std::string t) {
 }
 
 //method to iterate over goals and print current list under Category
-void printList() {
+void Category::printList() {
     std::cout<<name<<std::endl;
     //call method that underlines the category title depending on letters in name 
     for (const auto task: goals) {
@@ -24,7 +26,7 @@ void printList() {
     }
 }
 //method to delete a Task in goals (make sure to call method that checks if task exists in list before deleting)
-void deleteTasks(int n) {
+void Category::deleteTasks(int n) {
     if (goalsEmpty() || n<=0 || n>goals.size()) {
         std::cout<<"You have no tasks created yet or have entered an invalid number."<<std::endl;
     }
@@ -35,16 +37,27 @@ void deleteTasks(int n) {
 }
 
 //method to mark a Task completed 
+void Category::completeTask(int n) {
+    if (goalsEmpty() || n<=0 || n>goals.size()) {
+        std::cout<<"You have no tasks created yet or have entered an invalid number."<<std::endl;
+    }
+    //need to locate the task specified
+    auto it=goals.being(); 
+    std::advance(it, n-1);
+    //calling task object method to mark complete
+    it->markComplete();
+
+}
 
 //ERROR HANDLING METHODS: 
 
 //method to check if goals is empty 
-bool goalsEmpty() {
+bool Category::goalsEmpty() {
     return goals.empty();
 }
 
 //method to check if all Tasks have been completed 
-bool taskComplete() {
+bool Category::taskComplete() {
     int complete=0;
     for (const auto t: goals) {
         if (getCompletion()) {
@@ -59,7 +72,7 @@ bool taskComplete() {
 }
 
 //method to check if task already exists in list, if so return true if not return false 
-bool taskExist(std::string input) {
+bool Category::taskExist(std::string input) {
     for (const auto i : goals) {
         if (input==i.getTask()) {
             return true;
